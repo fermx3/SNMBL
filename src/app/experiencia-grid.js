b69d1989f97e8c2f441b3569fc23188399cf4e93
Fernando Rios <fernandomanuelriosalcantara@iMac-de-Fernando.local>
2023-03-21 15:31:16 -0600

    Add assets webpack support, html loader & cleanWebpack plugin

--- src/app/experiencia-grid.js
+++ src/app/experiencia-grid.js
@@ -1,49 +1,74 @@
+import danone from '../assets/images/brands/danone.png';
+import quaker from '../assets/images/brands/quaker.png';
+import kelloggs from '../assets/images/brands/kelloggs.png';
+import modelo from '../assets/images/brands/modelo.png';
+import lala from '../assets/images/brands/lala.png';
+import laCostena from '../assets/images/brands/la-costena.png';
+import washington from '../assets/images/brands/washington.png';
+import doritos from '../assets/images/brands/doritos.png';
+import crackets from '../assets/images/brands/crackets.png';
+import sevenUp from '../assets/images/brands/7up.png';
+import nike from '../assets/images/brands/nike.png';
+import nestle from '../assets/images/brands/nestle.png';
+
 export function experienciaGrid() {
   const experienciaGrid = document.getElementById('experiencia-grid');
 
   const brands = [
     {
       name: 'danone',
+      image: danone,
     },
     {
       name: 'quaker',
+      image: quaker,
     },
     {
       name: 'kelloggs',
+      image: kelloggs,
     },
     {
       name: 'modelo',
+      image: modelo,
     },
     {
       name: 'lala',
+      image: lala,
     },
     {
       name: 'la-costena',
+      image: laCostena,
     },
     {
       name: 'washington',
+      image: washington,
     },
     {
       name: 'doritos',
+      image: doritos,
     },
     {
       name: 'crackets',
+      image: crackets,
     },
     {
       name: '7up',
+      image: sevenUp,
     },
     {
       name: 'nike',
+      image: nike,
     },
     {
       name: 'nestle',
+      image: nestle,
     },
   ];
 
   brands.forEach((brand) => {
     const brandLogo = document.createElement('div');
     brandLogo.innerHTML = `
-    <img src="src/images/brands/${brand.name}.png" alt=${brand.name} />
+    <img src=${brand.image} alt=${brand.name} />
       `;
     brandLogo.className = 'brand';
     experienciaGrid.appendChild(brandLogo);
---
+++ webpack.common.js
@@ -0,0 +1,60 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/imgs/[name].[ext]',
+        },
+      },
+      {
+        test: /\.mp4$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/videos/[name].[hash][ext]',
+        },
+      },
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        type: 'asset',
+        generator: {
+          filename: 'assets/imgs/[name].[hash][ext]',
+        },
+      },
+      {
+        test: /\.(otf)$/,
+        type: 'asset',
+        generator: {
+          filename: 'assets/fonts/[name].[hash][ext]',
+        },
+      },
+    ],
+  },
+};
